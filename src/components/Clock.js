5ab1b49899891afd0c5963fa4c41b8a107841d8b
unknown <jaeheon.dev@gmail.com>
2023-01-30 21:47:03 +0900

    test upload
    ;

---
+++ src/components/Clock.js
@@ -0,0 +1,16 @@
+import React from "react";
+import styled from "styled-components";
+
+const Container = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+`;
+
+export default function Clock(){
+  return(
+    <Container>
+      Hello
+    </Container>
+  );
+}
